0a542c5244551489e88c3fb933aa4f72c943b448
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-22 21:27:31 +0000

    updates

--- lib/screens/review_screen.dart
+++ lib/screens/review_screen.dart
@@ -1,103 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:laza/widgets/auth_text_field.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:laza/widgets/new_custom_textfield.dart';
 import 'package:laza/widgets/navigation_card.dart';
+import 'package:laza/widgets/new_custom_textfield.dart';
+
 import '../widgets/custom_back_button.dart';
 
-void main(List<String> args) {
-  runApp(const ReviewScreen());
-}
-
-class ReviewScreen extends StatelessWidget {
+class ReviewScreen extends StatefulWidget {
   static const routeName = '/review';
 
   const ReviewScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme;
-    TextEditingController nameController =TextEditingController();
+  State<ReviewScreen> createState() => _ReviewScreenState();
+}
 
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      home: Scaffold(
+class _ReviewScreenState extends State<ReviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double sliderValue = 2.5;
+
+    final color = Theme.of(context).colorScheme;
+    TextEditingController nameController = TextEditingController();
+    TextEditingController describeExperienceController =
+        TextEditingController();
+
+    return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(backgroundColor: Colors.transparent,
-      elevation: 0,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         leading: Padding(
           padding: const EdgeInsets.only(left: 13.0),
           child: CustomBackButton(
             backgroundColor: color.background,
           ),
-    
         ),
         centerTitle: true,
-        title: Text('Add Review',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w600,color: color.secondary),),
+        title: Text(
+          'Add Review',
+          style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w600,
+              color: color.secondary),
+        ),
       ),
-
-    body: Column( 
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-           
-            children: [
-              Text('Name',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-            SizedBox(height: 10,),
-        
-            CustomTextfield(
-                controller: nameController,
-                labelText:'Type your name',
-                verticalLength: 50,
-                horizontalLength: 400,
-                
-                     ),
-            SizedBox(height: 30,),
-               Text('How was your experience ?',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-            SizedBox(height: 10,),
-               CustomTextfield(
-                controller: nameController,
-                labelText:'Describe your experience',
-                verticalLength: 213,
-                horizontalLength: 400,
-                
-                     ),
-            
-            SizedBox(height: 20,),
-            Text('Star',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-            SizedBox(height: 15,),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-
-                Text('0.0',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-                Stack(clipBehavior: Clip.none,
-                  children: [
-                   Image.asset('assets/images/Rectangle.png',),
-                   
-                    Positioned(right: 120,
-                      child:Image.asset('assets/images/Ellipse.png',), )
-                  ],
+                const Text(
+                  'Name',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
                 ),
-                     
-                    Text('5.0',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-
-
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 30),
+                  child: CustomTextfield(
+                    fixedHeight: 40,
+                    controller: nameController,
+                    hintText: 'Type your name',
+                    contentPadding: const EdgeInsets.only(
+                        left: 15, right: 15, top: 0, bottom: 0),
+                  ),
+                ),
+                const Text(
+                  'How was your experience ?',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 20),
+                  child: CustomTextfield(
+                    fixedHeight: 40,
+                    controller: describeExperienceController,
+                    hintText: 'Describe your experience',
+                    contentPadding: const EdgeInsets.only(
+                        left: 15, right: 15, top: 15, bottom: 0),
+                  ),
+                ),
+                const Text(
+                  'Star',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      '0.0',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                    ),
+                    Expanded(
+                      child: Slider(
+                        value: sliderValue,
+                        min: 0.0,
+                        max: 5.0,
+                        onChanged: (value) {
+                          setState(() {
+                            sliderValue = value;
+                          });
+                        },
+                      ),
+                    ),
+                    const Text(
+                      '5.0',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                )
               ],
             ),
-        
-        
-            
-            
-            ],
           ),
-        ),
-                 SizedBox(height: 157,),
-     NavigationCard(
-                          text: 'Submit Review',
-                         onTap: (){}),
-      ],
-    ),
-    ),
+          const Spacer(),
+          NavigationCard(text: 'Submit Review', onTap: () {}),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/new_custom_textfield.dart
+++ lib/widgets/new_custom_textfield.dart
@@ -4,36 +4,37 @@ class CustomTextfield extends StatelessWidget {
   const CustomTextfield({
     Key? key,
     required this.controller,
-    required this.labelText,
-    required this.verticalLength,
-    required this.horizontalLength,
+    required this.hintText,
+    required this.contentPadding,
+    required this.fixedHeight,
   }) : super(key: key);
 
   final TextEditingController controller;
-  final String labelText;
-  final double verticalLength; // Change the type to double for height
-  final double horizontalLength; // Change the type to double for width
+  final String hintText;
+  final EdgeInsetsGeometry contentPadding;
+  final double fixedHeight;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: horizontalLength, // Use horizontalLength for width
-      height: verticalLength, // Use verticalLength for height
-      padding: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-       color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(15.0),
-      ),
+    final color = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      height: fixedHeight,
       child: TextField(
-        
         controller: controller,
         keyboardType: TextInputType.emailAddress,
+        textAlignVertical: TextAlignVertical.top,
+        maxLines: null,
         decoration: InputDecoration(
-          labelText: labelText,
-          labelStyle: const TextStyle(
-            color: Color.fromRGBO(143, 149, 158, 1),
+          contentPadding: contentPadding,
+          filled: true,
+          fillColor: color.background,
+          hintText: hintText,
+          hintStyle: TextStyle(color: color.tertiary),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(10),
           ),
-          border: InputBorder.none,
         ),
       ),
     );
