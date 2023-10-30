3c2ff535b64de02c08fae022754e850042392782
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-30 01:14:14 +0000

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:laza/firebase_options.dart';
 import 'package:laza/screens/onboarding_screen.dart';
 
 import 'consts/app_routes.dart';
@@ -17,16 +19,29 @@ void main() {
   });
 }
 
+Future<FirebaseApp> firebaseInitialization = Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: themeData(),
-      debugShowCheckedModeBanner: false,
-      routes: AppRoutes().getRoutes(),
-      initialRoute: OnboardingScreen.routeName,
-    );
+    return FutureBuilder<FirebaseApp>(
+        future: firebaseInitialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something went wrong!'),
+            );
+          }
+          return MaterialApp(
+            theme: themeData(),
+            debugShowCheckedModeBanner: false,
+            routes: AppRoutes().getRoutes(),
+            initialRoute: OnboardingScreen.routeName,
+          );
+        });
   }
 }
