a41fa786db2f3310c9fc9f4a1e6f76b732ebc20d
bnasare <124492150+bnasare@users.noreply.github.com>
2023-11-11 18:20:24 +0000

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -4,42 +4,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:laza/firebase_options.dart';
-import 'package:laza/provider/internet_provider.dart';
-import 'package:laza/provider/sign_in_provider.dart';
-import 'package:laza/screens/authentication/screens/signup_screen.dart';
-import 'package:laza/screens/authentication/screens/social_auth_screen.dart';
-import 'package:laza/screens/onboarding_screen.dart';
-import 'package:provider/provider.dart';
+import 'package:laza/providers/internet_provider.dart';
+import 'package:laza/providers/sign_in_provider.dart';
 import 'package:laza/screens/home_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:laza/screens/home_screen.dart';
-import 'package:provider/provider.dart';
-import 'package:laza/screens/home_screen.dart';
+
 import 'consts/app_routes.dart';
 import 'consts/theme.dart';
-import 'firebase_options.dart';
 import 'providers/cart_provider.dart';
 import 'providers/product_provider.dart';
 import 'providers/wishlist_provider.dart';
-import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     GoogleFonts.config.allowRuntimeFetching = false;
     runApp(
       DevicePreview(
         enabled: true,
-        builder: (context) => const MyApp(),
+        builder: (context) => MyApp(),
       ),
     );
   });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final firebaseInitialization =
+      Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   @override
   Widget build(BuildContext context) {
