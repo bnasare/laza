d51e32ea0c08ceac8e7cced01fc4b84bb421a144
bnasare <124492150+bnasare@users.noreply.github.com>
2023-11-21 19:43:40 +0000

    updates

--- lib/screens/authentication/screens/signup_screen.dart
+++ lib/screens/authentication/screens/signup_screen.dart
@@ -3,12 +3,10 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:laza/screens/authentication/screens/login_screen.dart';
 
 import '../../../widgets/cards/bottom_card.dart';
 import '../../../widgets/custom icons/custom_back_button.dart';
-import '../../../widgets/switch.dart';
 import '../widgets/auth_text_field.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -26,6 +24,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController fullNameController = TextEditingController();
 
   Future<void> signUp() async {
+    if (!mounted) {
+      return; // Check if the widget is still mounted
+    }
+
     if (emailController.text.isEmpty ||
         passwordController.text.isEmpty ||
         fullNameController.text.isEmpty) {
@@ -40,7 +42,11 @@ class _SignUpScreenState extends State<SignUpScreen> {
           email: emailController.text.toLowerCase().trim(),
           password: passwordController.text.trim());
       final User? user = FirebaseAuth.instance.currentUser;
-      final uid = user!.uid;
+      if (user == null || !mounted) {
+        return; // Check again if the widget is still mounted
+      }
+
+      final uid = user.uid;
       user.updateDisplayName(fullNameController.text);
       user.reload();
       await FirebaseFirestore.instance.collection('users').doc(uid).set({
@@ -51,17 +57,28 @@ class _SignUpScreenState extends State<SignUpScreen> {
         'userCartItems': [],
         'createdAt': Timestamp.now(),
       });
-      Navigator.pushReplacement(context,
-          MaterialPageRoute(builder: (context) => const LoginScreen()));
+
+      if (mounted) {
+        // Check one more time before navigating
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const LoginScreen()));
+      }
+
       log('Successfully registered');
     } on FirebaseException catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(error.message.toString())),
-      );
+      if (mounted) {
+        // Check before showing the snackbar
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(error.message.toString())),
+        );
+      }
     } finally {
-      emailController.clear();
-      passwordController.clear();
-      fullNameController.clear();
+      if (mounted) {
+        // Clear controllers only if the widget is still mounted
+        emailController.clear();
+        passwordController.clear();
+        fullNameController.clear();
+      }
     }
   }
 
@@ -81,47 +98,45 @@ class _SignUpScreenState extends State<SignUpScreen> {
       ),
       body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  "Sign Up",
-                  style: TextStyle(
-                    color: color.secondary,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: -0.21,
+          child: Column(children: [
+            Text(
+              "Sign Up",
+              style: TextStyle(
+                color: color.secondary,
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+                letterSpacing: -0.21,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 180),
+              child: Column(children: [
+                AuthTextField(
+                  textInputAction: TextInputAction.next,
+                  controller: fullNameController,
+                  labelText: "Username",
+                  trailingWidget: Icon(
+                    Icons.check_outlined,
+                    color: color.onSecondary,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 35),
-                  child: Column(children: [
-                    AuthTextField(
-                      textInputAction: TextInputAction.next,
-                      controller: fullNameController,
-                      labelText: "Username",
-                      trailingWidget: Icon(
-                        Icons.check_outlined,
-                        color: color.onSecondary,
-                      ),
-                    ),
-                    AuthTextField(
-                      textInputAction: TextInputAction.next,
-                      controller: passwordController,
-                      labelText: "Password",
-                    ),
-                    AuthTextField(
-                      textInputAction: TextInputAction.done,
-                      controller: emailController,
-                      labelText: "Email Address",
-                      trailingWidget: Icon(
-                        Icons.check_outlined,
-                        color: color.onSecondary,
-                      ),
-                    ),
-                  ]),
-                )
-              ])),
+                AuthTextField(
+                  textInputAction: TextInputAction.next,
+                  controller: passwordController,
+                  labelText: "Password",
+                ),
+                AuthTextField(
+                  textInputAction: TextInputAction.done,
+                  controller: emailController,
+                  labelText: "Email Address",
+                  trailingWidget: Icon(
+                    Icons.check_outlined,
+                    color: color.onSecondary,
+                  ),
+                ),
+              ]),
+            )
+          ])),
       bottomNavigationBar: NavigationCard(
           text: 'Sign Up',
           onTap: () async {
--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -34,7 +34,7 @@ class OnboardingScreen extends StatelessWidget {
                 bottom: 0,
                 child: Container(
                   padding: const EdgeInsets.only(
-                      top: 25.0, bottom: 5, left: 12, right: 12),
+                      top: 20.0, bottom: 5, left: 12, right: 12),
                   width: horizontalConverter(context, 345),
                   height: 244,
                   decoration: BoxDecoration(
