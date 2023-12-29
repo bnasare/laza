5d464c4700d5baf620a2ea7c032ee9539a615f6b
Sena <emmanueldokeii@gmail.com>
2023-12-29 05:37:22 +0000

    fixed product and brand bugs

--- lib/screens/brand_screen.dart
+++ lib/screens/brand_screen.dart
@@ -7,6 +7,7 @@ import 'package:laza/widgets/cards/product_widget.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/product_provider.dart';
+import '../utils/snack_bar.dart';
 import '../widgets/custom icons/custom_back_button.dart';
 import '../widgets/custom icons/custom_trailing_button.dart';
 
@@ -20,14 +21,9 @@ class BrandScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context).colorScheme;
-    final productProvider = Provider.of<ProductProvider>(context);
-    List<ProductModel> products = productProvider.getProducts;
+    final productProvider = Provider.of<ProductProvider>(context, listen: false);
+    List<ProductModel> products = productProvider.productsList;
     List<ProductModel> brandProducts = [];
-    for (ProductModel product in products) {
-      if (product.category == brand) {
-        brandProducts.add(product);
-      }
-    }
 
     return Scaffold(
       appBar: AppBar(
@@ -60,83 +56,103 @@ class BrandScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: horizontalConverter(context, 20),
-        ),
-        child: Column(
-          children: [
-            ListTile(
-              contentPadding: const EdgeInsets.all(0),
-              title: Text(
-                '${brandProducts.length} Items',
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: color.secondary,
-                    fontSize: 17),
-              ),
-              subtitle: Text(
-                'Available in stock',
-                style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                    color: color.tertiary),
-              ),
-              trailing: Container(
-                width: horizontalConverter(context, 70),
-                height: verticalConverter(context, 37),
-                decoration: BoxDecoration(
-                    color: color.background,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Icon(
-                      CupertinoIcons.sort_down,
-                      size: 20,
-                      color: color.secondary,
-                    ),
-                    Text(
-                      'Sort',
-                      style: TextStyle(
+      body: FutureBuilder(
+        future: productProvider.fetchProducts(),
+        builder: (context, snapshot) {
+          brandProducts.clear();
+          for (ProductModel product in products) {
+            if (product.category.toLowerCase() == brand) {
+              brandProducts.add(product);
+            }
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SizedBox();
+          } else if (snapshot.hasError) {
+            openSnackbar(
+              context,
+              'Error: ${snapshot.error}',
+              color,
+            );
+          }
+          return Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: horizontalConverter(context, 20),
+            ),
+            child: Column(
+              children: [
+                ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  title: Text(
+                    '${brandProducts.length} Items',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: color.secondary,
+                        fontSize: 17),
+                  ),
+                  subtitle: Text(
+                    'Available in stock',
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                        color: color.tertiary),
+                  ),
+                  trailing: Container(
+                    width: horizontalConverter(context, 70),
+                    height: verticalConverter(context, 37),
+                    decoration: BoxDecoration(
+                        color: color.background,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          CupertinoIcons.sort_down,
+                          size: 20,
                           color: color.secondary,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 15),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: 1,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding:
-                        EdgeInsets.only(top: verticalConverter(context, 20)),
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 10.0,
-                        childAspectRatio: 0.62,
-                      ),
-                      itemCount: brandProducts.length,
-                      itemBuilder: (context, index) {
-                        return ChangeNotifierProvider.value(
-                            value: brandProducts[index],
-                            child: const ProductWidget());
-                      },
+                        ),
+                        Text(
+                          'Sort',
+                          style: TextStyle(
+                              color: color.secondary,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 15),
+                        )
+                      ],
                     ),
-                  );
-                },
-              ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 1,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: EdgeInsets.only(
+                            top: verticalConverter(context, 20)),
+                        child: GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 10.0,
+                            childAspectRatio: 0.62,
+                          ),
+                          itemCount: brandProducts.length,
+                          itemBuilder: (context, index) {
+                            return ChangeNotifierProvider.value(
+                                value: brandProducts[index],
+                                child: const ProductWidget());
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
